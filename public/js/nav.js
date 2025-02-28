f609f7fcc92e947bbb28d793f7ad3aca8fab0e6b
Justin <snakejsj9@gmail.com>
2025-02-28 00:33:46 -0800

    navbar toggle, UX/UI

---
+++ public/js/nav.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById('overlay').classList.add('overlay');
+    document.getElementById('nav').classList.add('open')
+}
+
+function closeNav() {
+    document.getElementById('overlay').classList.remove('overlay');
+    document.getElementById('nav').classList.remove('open')
+}
